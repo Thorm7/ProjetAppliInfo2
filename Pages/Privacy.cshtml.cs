89b32ee379f72be39e380c185e1ffa2241ae8a2a
Thorm7 <thomas.romain@condorcet.be>
2026-05-14 12:02:43 +0200

    Premier commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LiberNet.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
